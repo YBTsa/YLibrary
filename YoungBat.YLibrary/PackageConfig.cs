cfc24fc87be88ca78d8495a76d997e9daba9ce59
YBTsa <13703503153@163.com>
2025-05-31 16:37:11 +0800

    添加项目文件。

---
+++ YoungBat.YLibrary/PackageConfig.cs
@@ -0,0 +1,10 @@
+﻿namespace YoungBat.YLibrary
+{
+    public class PackageConfig(string name, string description, string author, List<string> files)
+    {
+        public string Name { get; } = name ?? throw new ArgumentNullException(nameof(name));
+        public string Description { get; } = description ?? throw new ArgumentNullException(nameof(description));
+        public string Author { get; } = author ?? throw new ArgumentNullException(nameof(author));
+        public IReadOnlyList<string> Files { get; } = (files ?? throw new ArgumentNullException(nameof(files))).AsReadOnly();
+    }
+}
---
+++ YoungBat.YLibrary/PackageManager.cs
@@ -0,0 +1,7 @@
+﻿namespace YoungBat.YLibrary
+{
+    public class PackageManager(string packagePath)
+    {
+        private string _packagePath = packagePath;
+    }
+}
