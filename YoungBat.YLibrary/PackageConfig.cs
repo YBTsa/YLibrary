7a0e7b2942a01db9737e9d2fe005d974c95c8f70
YBTsa <13703503153@163.com>
2025-06-01 10:26:36 +0800

    #

--- YoungBat.YLibrary/PackageConfig.cs
+++ YoungBat.YLibrary/PackageConfig.cs
@@ -1,10 +1,29 @@
 ﻿namespace YoungBat.YLibrary
 {
+    /// <summary>
+    /// Easy Package Configuration Class for Package Class and Package Manager Class
+    /// </summary>
+    /// <param name="name">Package Name,can't be null</param>
+    /// <param name="description">Package Description,can't be null</param>
+    /// <param name="author">Package Author,can't be null</param>
+    /// <param name="files">Package Files List,can't be null</param>
     public class PackageConfig(string name, string description, string author, List<string> files)
     {
+        /// <summary>
+        /// Package Name
+        /// </summary>
         public string Name { get; } = name ?? throw new ArgumentNullException(nameof(name));
+        /// <summary>
+        /// Package Description
+        /// </summary>
         public string Description { get; } = description ?? throw new ArgumentNullException(nameof(description));
+        /// <summary>
+        /// Package Author
+        /// </summary>
         public string Author { get; } = author ?? throw new ArgumentNullException(nameof(author));
+        /// <summary>
+        /// Package Files List
+        /// </summary>
         public IReadOnlyList<string> Files { get; } = (files ?? throw new ArgumentNullException(nameof(files))).AsReadOnly();
     }
 }
--- YoungBat.YLibrary/PackageManager.cs
+++ YoungBat.YLibrary/PackageManager.cs
@@ -1,7 +1,11 @@
 ﻿namespace YoungBat.YLibrary
 {
+    /// <summary>
+    /// Package manager class
+    /// </summary>
+    /// <param name="packagePath">Location of the package</param>
     public class PackageManager(string packagePath)
     {
-        private string _packagePath = packagePath;
+        private readonly string _packagePath = packagePath;
     }
 }
